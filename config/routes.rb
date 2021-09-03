81aac80595cbf6b5303fa402cd795907ce9bc83e
paopapaopao <paolo.casugay@gmail.com>
2021-09-03 22:53:21 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'pending/index'
   # match "/users", to: "users#index", via: "get"
+
+  get "/get_price/:symbol" => "stocks#get_price", as: :get_price
 
   resources :roles
   devise_for :users
@@ -13,5 +17,8 @@ Rails.application.routes.draw do
   root to: "stocks#index"
 
   post "create_user" => "users#create", as: :create_user
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticate :user, lambda { |u| u.isadmin == true } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
 end
