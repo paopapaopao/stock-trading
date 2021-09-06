61a434ed5232371a9bd65955c529c7109f564744
paopapaopao <paolo.casugay@gmail.com>
2021-09-06 19:49:42 +0800

    Add symbol column to stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_022654) do
+ActiveRecord::Schema.define(version: 2021_09_06_091818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_022654) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "symbol"
     t.index ["user_id"], name: "index_stocks_on_user_id"
   end
 
